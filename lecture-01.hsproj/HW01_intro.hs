5f26b92081ccbaf1291b0d9fb618ffd583fdd6eb
Edvin CANDON <edvincandon@gmail.com>
2018-10-04 17:08:44 +0200

    Extra HW - Hanoi Tower 4 pegs

--- lecture-01.hsproj/HW01_intro.hs
+++ lecture-01.hsproj/HW01_intro.hs
@@ -7,7 +7,7 @@ toDigits n
   | otherwise = (toDigits (n `div` 10)) ++ [n `mod` 10] 
   
 toDigitsReverse :: Integer -> [Integer]
-toDigitsReverse n = (reverse.toDigits) n
+toDigitsReverse n = (reverse . toDigits) n
   
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther l = [if i `mod` 2 == 0
@@ -29,6 +29,17 @@ hanoi n source aux dest
   | otherwise = hanoi (n-1) source dest aux
     ++ [(source, dest)]
     ++ hanoi (n-1) aux source dest
+
+
+-- Frame–Stewart algorithm
+-- https://en.wikipedia.org/wiki/Tower_of_Hanoi#With_four_pegs_and_beyond
+hanoiFour :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoiFour n source aux1 aux2 dest
+  | n == 0 = []
+  | otherwise = hanoiFour k source aux1 dest aux2
+    ++ hanoi (n - k) source aux1 dest
+    ++ hanoiFour k aux2 aux1 source dest
+  where k = (n - round(sqrt(2.0 * fromIntegral n + 1.0)) + 1)
 
 
 
